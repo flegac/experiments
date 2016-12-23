7651bb0e716cdb65a1d26315762b38a20fb85644
flegac <florent.legac@gmail.fr>
2016-12-23 12:43:51 +0100

    feat(ui) : minor updates

--- src/main/java/fr/flegac/experiments/ui/context/UIContext.java
+++ src/main/java/fr/flegac/experiments/ui/context/UIContext.java
@@ -30,22 +30,6 @@ public class UIContext {
         return components.get(id);
     }
 
-    public String serializedComponents() {
-        UIComponentSet componentSet = new UIComponentSet();
-        components.values().forEach(item -> {
-            if (item instanceof UILabel) {
-                componentSet.labels.add((UILabel) item);
-            }
-            else if (item instanceof UIRessource) {
-                componentSet.resources.add((UIRessource) item);
-            }
-            else if (item instanceof UIBox) {
-                componentSet.boxes.add((UIBox) item);
-            }
-        });
-        return gson.toJson(componentSet);
-    }
-
     public void loadComponents(String filename) throws FileNotFoundException {
         UIComponentSet componentSet = parseComponents(new FileReader(filename));
         componentSet.labels.stream().forEach(item -> {
@@ -67,16 +51,32 @@ public class UIContext {
         views.put(view.id, view);
     }
 
+    public Viewport getViewport(String id) {
+        return viewports.get(id);
+    }
+
+    public String serializedComponents() {
+        UIComponentSet componentSet = new UIComponentSet();
+        components.values().forEach(item -> {
+            if (item instanceof UILabel) {
+                componentSet.labels.add((UILabel) item);
+            }
+            else if (item instanceof UIRessource) {
+                componentSet.resources.add((UIRessource) item);
+            }
+            else if (item instanceof UIBox) {
+                componentSet.boxes.add((UIBox) item);
+            }
+        });
+        return gson.toJson(componentSet);
+    }
+
     private UIComponentSet parseComponents(Reader reader) {
         return gson.fromJson(reader, UIComponentSet.class);
     }
 
     private UIView parseView(Reader reader) {
         return gson.fromJson(reader, UIView.class);
-    }
-
-    public Viewport getViewport(String id) {
-        return viewports.get(id);
     }
 
 }
